0ce7b5176b313e255fabdd423c3f1db476a39fe6
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2023-07-31 13:46:22 -0400

    Add feed follows

---
+++ handler_feed_follows.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"github.com/lf-hernandez/go-rss-aggregator/internal/database"
+)
+
+func (apiConfiguration *apiConfig) handlerCreateFeedFollow(responseWriter http.ResponseWriter, request *http.Request, authenticatedUser database.User) {
+	type parameteres struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+
+	decoder := json.NewDecoder(request.Body)
+
+	params := parameteres{}
+	decoderError := decoder.Decode(&params)
+
+	if decoderError != nil {
+		respondWithError(responseWriter, 400, fmt.Sprintln("Error parsing JSON: ", decoderError))
+		return
+	}
+
+	feedFollow, createFeedError := apiConfiguration.DB.CreateFeedFollow(request.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    authenticatedUser.ID,
+		FeedID:    params.FeedID,
+	})
+
+	if createFeedError != nil {
+		respondWithError(responseWriter, 400, fmt.Sprintln("Couldn't create feed follow:, ", createFeedError))
+		return
+	}
+
+	respondWithJSON(responseWriter, 201, databaseFeedFollowToFeedFollow(feedFollow))
+}
+
+func (apiConfiguration *apiConfig) handlerGetFeedFollows(responseWriter http.ResponseWriter, request *http.Request, authenticatedUser database.User) {
+	feedFollows, getFeedsError := apiConfiguration.DB.GetFeedFollows(request.Context(), authenticatedUser.ID)
+	if getFeedsError != nil {
+		respondWithError(responseWriter, 400, fmt.Sprintln("Couldn't get feeds follows, ", getFeedsError))
+		return
+	}
+
+	respondWithJSON(responseWriter, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(responseWriter http.ResponseWriter, request *http.Request, authenticatedUser database.User) {
+	feedFollowIDString := chi.URLParam(request, "feedFollowID")
+	feedFollowID, uuidParsingError := uuid.Parse(feedFollowIDString)
+	if uuidParsingError != nil {
+		respondWithError(responseWriter, 400, fmt.Sprintln("Couldn't parse feed follow id: ", uuidParsingError))
+	}
+
+	databaseDeleteError := apiCfg.DB.DeleteFeedFollow(request.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: authenticatedUser.ID,
+	})
+
+	if databaseDeleteError != nil {
+		respondWithError(responseWriter, 400, fmt.Sprintln("Couldn't delete feed follow: ", databaseDeleteError))
+	}
+
+	respondOk(responseWriter)
+}
